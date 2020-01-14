7dca4c850b6beef68420dad36ce8bbdf95677d13
Lakpa Ringi Sherpa <lakpa_yaso@hotmail.com>
2020-01-14 14:42:55 +0545

    added displaying current location of the user

--- app/src/main/java/com/example/gmaps/MapsActivity.java
+++ app/src/main/java/com/example/gmaps/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.example.gmaps;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,14 +20,22 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.gmaps.Interface.MapsApi;
 import com.example.gmaps.Url.Url;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -33,20 +48,34 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener
+        {
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
     private AutoCompleteTextView etCity;
     private Button btnSearch;
+    private LocationRequest locationRequest;
     private List<LatitudeLongitude> latitudeLongitudeList;
-    Marker markerName;
+    private Marker currentUserLocationMarker;
+    private static final int Request_user_location_code = 99;
     CameraUpdate center, zoom;
-    private Location currentLocation;
+    private Location lastLocation;
+
     private static final String TAG = MapsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkUserLocationPermission();
+        }
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -96,6 +125,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+
+        }
 
         try {
             boolean success = googleMap.setMapStyle(
@@ -163,4 +200,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }*/
 
     }
-}
+
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                switch (requestCode){
+                    case Request_user_location_code:
+                        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) ==PackageManager.PERMISSION_GRANTED){
+                                if(googleApiClient==null){
+                                    buildGoogleApiClient();
+                                }
+                                mMap.setMyLocationEnabled(true);
+                            }
+                        }else{
+                            Toast.makeText(this,"Permission denied...",Toast.LENGTH_LONG).show();
+                       }
+                        return;
+                }
+            }
+
+            protected  synchronized  void buildGoogleApiClient(){
+                googleApiClient = new GoogleApiClient.Builder(this)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+            }
+
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                locationRequest = new LocationRequest();
+                locationRequest.setInterval(1100);
+                locationRequest.setFastestInterval(1100);
+                locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                    LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
+                }
+
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+
+            }
+
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                    lastLocation = location;
+                    if(currentUserLocationMarker!=null){
+                        currentUserLocationMarker.remove();
+                    }
+
+                    LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title("Your current location");
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+                    currentUserLocationMarker = mMap.addMarker(markerOptions);
+
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomBy(12));
+
+                    if(googleApiClient!=null){
+                        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,this);
+                    }
+
+            }
+
+            public boolean checkUserLocationPermission(){
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},Request_user_location_code);
+                    }else{
+                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},Request_user_location_code);
+                    }
+
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
